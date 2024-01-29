a948ef902fc5f009676fffbb95fa552c9064d518
karimim <muhammad.karimi@post.ch>
2024-01-29 15:59:37 +0100

    udate games

--- src/app/components/games.jsx
+++ src/app/components/games.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import { getData } from '../../api/api-client';
+import { getGames } from '../../api/api-client';
 import Platforms from './Platforms';
 
 const GameCardList = () => {
@@ -9,7 +9,7 @@ const GameCardList = () => {
 
   useEffect(() => {
     const fetchGames = async () => {
-      const data = await getData('games');
+      const data = await getGames();
       if (data) {
         setGames(data.results);
       }
@@ -18,22 +18,13 @@ const GameCardList = () => {
     fetchGames();
   }, []);
 
-  const resizedImageUrl = (imageUrl) => {
-    const target = 'media/';
-    const position = imageUrl.lastIndexOf(target) + target.length;
-    const text = 'resize/640/-/';
-    if (imageUrl) {
-      return imageUrl.slice(0, position) + text + imageUrl.slice(position);
-    }
-  }
-
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {games.map((game) => (
         <div key={game.id} className="bg-gray-200 rounded-md overflow-hidden">
           <div className="card-image overflow-hidden relative aspect-video">
             <Image 
-              src={resizedImageUrl(game.background_image)} 
+              src={game.background_image} 
               alt={game.name}
               layout="fill"
               sizes="25vw"
@@ -42,9 +33,7 @@ const GameCardList = () => {
           <div className="mx-2 my-3 flex flex-col justify-between">
             <h5 className="text-2xl">{game.name}</h5>
             <p className="text-sm text-gray-600">{game.released}</p>
-            {game.parent_platforms && 
-              <Platforms parentPlatforms={game.parent_platforms} />
-            }
+            <Platforms platforms={game.platforms} />
           </div>
         </div>
       ))}
